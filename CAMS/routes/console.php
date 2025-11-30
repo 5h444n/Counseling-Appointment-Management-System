f0396cf4134794db8ac7f21f395c3d8877e2bd46
5h44n <ahnaf430@gmail.com>
2025-11-30 08:23:22 +0600

    config: Schedule appointments:autocancel to run every minute

--- CAMS/routes/console.php
+++ CAMS/routes/console.php
@@ -2,7 +2,11 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+// Run the auto-cancellation logic every minute
+Schedule::command('appointments:autocancel')->everyMinute();
